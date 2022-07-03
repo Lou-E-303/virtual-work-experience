d477da0dfca7ff449ceb209db8a7af65f3bd5ff2
Lou-E-303 <louiscowell404@gmail.com>
2022-07-03 19:45:43 +0100

    Updated JSON & tasks

--- tasks/task1b.js
+++ tasks/task1b.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const productList = JSON.parse(fs.readFileSync('../EngineeringChallenge/resources/productList.json', 'utf8'));
-const faultyBasket = [5067373267184, "",  "Ginsters Large Cornish Pasty 227G", "Tesco Imperator Carrot 450G", 5157373267184, 5067373267123, "Tesco Finest Honey Roast Ham 125G", 5057373267984, 505737326798,  ];
+const faultyBasket = [5067373267184, "",  "Ginsters Large Cornish Pasty 227G", "Lucky Aide Imperator Carrot 450G", 5157373267184, 5067373267123, "Lucky Aide Finest Honey Roast Ham 125G", 5057373267984, 505737326798,  ];
 
 console.log("\nThe total price of the basket is: £" + getTotalPriceOfFaultyBasket(faultyBasket) + "\n");
 
