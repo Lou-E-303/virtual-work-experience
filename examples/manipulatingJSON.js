898ebc166a6e783acf43dcc76ac9d8f0997c8a8a
Lou-E-303 <louiscowell404@gmail.com>
2022-07-03 19:00:01 +0100

    Added code example for JSON manipulation

---
+++ examples/manipulatingJSON.js
@@ -0,0 +1,18 @@
+// Import 'file system' library, which allows us to manipulate files
+
+const fs = require('fs');
+
+// Parse our JSON file to a JavaScript object we can manipulate
+
+const productList = JSON.parse(fs.readFileSync('../EngineeringChallenge/productList.json', 'utf8'));
+
+// Print our JS object to the console to prove that we've got it!
+
+// console.log(productList);
+
+// Now we can manipulate our object
+// For example if we just wanted just the price of the first product...
+
+const firstProductName = productList.products[0].name;
+const firstProductPrice = productList.products[0].price;
+console.log("The price of " + firstProductName + " is £" + firstProductPrice);
