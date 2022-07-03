20fd55e6d16f79be4f0672bbcc81d1f5408881d7
Lou-E-303 <louiscowell404@gmail.com>
2022-07-03 20:07:59 +0100

    Updated Task 1b

--- tasks/task1b.js
+++ tasks/task1b.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const productList = JSON.parse(fs.readFileSync('../EngineeringChallenge/resources/productList.json', 'utf8'));
-const faultyBasket = [5067373267184, "",  "Ginsters Large Cornish Pasty 227G", "Lucky Aide Imperator Carrot 450G", 5157373267184, 5067373267123, "Lucky Aide Finest Honey Roast Ham 125G", 5057373267984, 505737326798,  ];
+const faultyBasket = [5067373267184, "",  "Ginsters Large Cornish Pasty 227G", "Tesco Imperator Carrot 450G", 5157373267184, 5067373267123, "Tesco Finest Honey Roast Ham 125G", 5057373267984, 505737326798,  ];
 
 console.log("\nThe total price of the basket is: £" + getTotalPriceOfFaultyBasket(faultyBasket) + "\n");
 
@@ -19,11 +19,12 @@ function getTotalPriceOfFaultyBasket(faultyBasket) {
     return totalPrice.toFixed(2);
 }
 
-function getPriceByNameOrGtin(product) {
+function getPriceByNameOrGtin(productNameOrGtin) {
     for (let i = 0; i < productList.products.length; i++) {
-        let productElement = typeof product === 'string' ? productList.products[i].name : productList.products[i].gtin;
+        let product = productList.products[i];
+        let productElement = typeof productNameOrGtin === 'string' ? product.name : product.gtin;
 
-        if (productElement == product)
+        if (productElement == productNameOrGtin)
         {
             return productList.products[i].price;
         }
